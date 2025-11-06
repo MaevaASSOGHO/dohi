64259f3ac246a809a079d7714926ed84cf22dee8
MaevaASSOGHO <tabouretgangbusiness@gmail.com>
2025-11-06 11:29:49 +0100

    feed2

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,45 +1,6 @@
-// src/lib/api.js
+// src/lib/api.js (garde ça simple et stable)
 import axios from "axios";
-
-const BASE =
-  import.meta.env.VITE_API_BASE?.replace(/\/+$/, "") ||
-  "http://localhost:8000/api";
-
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE || BASE,
-  headers: { Accept: "application/json", "Content-Type": "application/json" },
-});
-
-
-
-// Ajoute automatiquement le Bearer si présent
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers = config.headers || {};
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-// 401 => purge token + redirection /login
-let alreadyRedirecting = false;
-api.interceptors.response.use(
-  (res) => res,
-  (err) => {
-    const status = err?.response?.status;
-    if (status === 401 && !alreadyRedirecting) {
-      alreadyRedirecting = true;
-      try { localStorage.removeItem("token"); } catch {}
-      const path = window.location.pathname || "";
-      if (!/\/(login|register)$/i.test(path)) {
-        window.location.assign("/login");
-      } else {
-        alreadyRedirecting = false;
-      }
-    }
-    return Promise.reject(err);
-  }
-);
-
+const BASE = (import.meta.env.VITE_API_BASE?.replace(/\/+$/, "")) || "http://localhost:8000";
+const api = axios.create({ baseURL: BASE, headers: { Accept: "application/json" } });
+// (interceptors Authorization comme tu avais)
 export default api;
